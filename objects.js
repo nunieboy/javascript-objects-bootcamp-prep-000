b0b1f7a509dd9d354b8ee5dd452d26067eaf282f
Pablo Eusebio <nunieboy88@gmail.com>
2018-08-28 02:29:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,13 @@ console.log(meal.breakfast)
 
 console.log(meal[firstMeal])
 
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value
+  return obj
+}
+
+const recipe = {eggs: 3}
+
+destructivelyUpdateObjectWithKeyAndValue(recipe, "flour", "3 cups")
+
+recipe
